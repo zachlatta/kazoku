a5f32ab07d57ee03b538cf63596c8b08184ef097
Zach Latta <zach@zachlatta.com>
2014-03-01 21:31:20 -0800

    Add tasks and check them off.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,39 @@
 angular.module('starter.controllers', [])
 
-.controller('TasksCtrl', function($scope, TaskService) {
+.controller('TasksCtrl', function($scope, $ionicModal, TaskService) {
   TaskService.$bind($scope, 'tasks');
+
+  $scope.rightButtons = [
+    {
+      type: 'button-clear',
+      content: 'Add',
+      tap: function (e) {
+        $scope.newTask();
+      }
+    }
+  ];
+
+  $ionicModal.fromTemplateUrl('templates/tasks-add.html', function (modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  $scope.newTask = function () {
+    $scope.taskModal.show();
+  };
+
+  $scope.closeNewTask = function () {
+    $scope.taskModal.hide();
+  };
+
+  $scope.createTask = function (task) {
+    TaskService.$add({
+      title: task.title,
+      completed: false
+    });
+    $scope.taskModal.hide();
+    task.title = '';
+  };
 })
